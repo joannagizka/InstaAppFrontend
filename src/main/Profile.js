7d6f55d2b1d1a32331e1dcaee0b10d7350611007
White Joan <joannnabiala@gmail.com>
2020-11-13 14:55:48 +0100

    Frontend: Profile.js refactored to Hooks.

--- src/main/Profile.js
+++ src/main/Profile.js
@@ -1,42 +1,31 @@
 import axios from 'axios';
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 
 
-export default class MyProfile extends Component {
+const Profile = () => {
+  const arr = window.location.href.split("/");
 
-  constructor(props) {
-    super(props);
+  const [username, setUsername] = useState('')
+  const [photos, setPhotos] = useState([])
+  const [userId, setUserId] = useState(arr[arr.length - 1])
+  const [isObserved, setIsObserved] = useState(false)
 
-    const arr = window.location.href.split("/");
-    const userId = arr[arr.length - 1];
 
-    this.state = {
-      username: "",
-      photos: [],
-      userId: userId,
-      isObserved: false,
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-    this.isFollowed = this.isFollowed.bind(this);
-  }
-
-  componentDidMount() {
-    axios.get("http://localhost:8000/profile/" + this.state.userId + "/").then(response => {
-      this.setState({
-        username: response.data.username,
-        isObserved: response.data.isObserved,
-        photos: response.data.photos,
-      });
+  useEffect(() => {
+    axios.get("http://localhost:8000/profile/" + userId + "/").then(response => {
+      setUsername(response.data.username)
+      setIsObserved(response.data.isObserved)
+      setPhotos(response.data.photos)
     })
-  }
+  }, [])
 
-  renderAllPhotos() {
+
+  const renderAllPhotos = () => {
     const renderedPhotos = [];
 
-    for (let photo of this.state.photos) {
-      renderedPhotos.push(this.renderPhoto(photo));
+    for (let photo of photos) {
+      renderedPhotos.push(renderPhoto(photo));
     }
 
     return (
@@ -46,7 +35,9 @@ export default class MyProfile extends Component {
     )
   }
 
-  renderPhoto(photo) {
+
+
+  const renderPhoto = (photo) => {
     const src = "http://localhost:8000/photo/" + photo.id + "/"
     const linkTo = "/photodetails/" + photo.id;
     return (
@@ -59,91 +50,95 @@ export default class MyProfile extends Component {
     )
   }
 
-  handleClick() {
-    const path = this.state.isObserved ? "unfollow/" : "follow/";
-    axios.get("http://localhost:8000/" + path + this.state.userId + "/").then(response => {
-      this.setState({isObserved: !this.state.isObserved});
+
+
+  const handleClick = () => {
+    const path = isObserved ? "unfollow/" : "follow/";
+    axios.get("http://localhost:8000/" + path + userId + "/").then(response => {
+      setIsObserved(!isObserved);
     })
   }
 
-  isFollowed() {
+
+
+  const isFollowed = () => {
     return (
-      <button type="button" className="btn btn-primary" onClick={this.handleClick} id={this.state.userId}>
-        {this.state.isObserved ? 'przestań obserwować' : 'obserwuj'}
+      <button type="button" className="btn btn-primary" onClick={handleClick} id={userId}>
+        {isObserved ? 'przestań obserwować' : 'obserwuj'}
       </button>
     );
   }
 
 
-  render() {
-    require('./MyProfileStyle.css');
-    require('./Style.css');
-    require('./MainStyle.css');
 
-    return (
-      <div>
-        <div className="layout">
-          <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div className="container">
-              <a
-                className="navbar-brand js-scroll-trigger"
-                href="/mainpageforloggedin">
-                WhiteWall
-              </a>
-              <button
-                className="navbar-toggler navbar-toggler-right"
-                type="button" data-toggle="collapse"
-                data-target="#navbarResponsive"
-                aria-controls="navbarResponsive"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-                Menu
-                <i className="fas fa-bars"></i>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                  <div className="btn-group">
-                    <Link to="/addphoto" className="btn bg-primary light">Dodaj zdjęcie</Link>
-                    <Link to="/logout" className="btn bg-primary light">Wyloguj się</Link>
+  require('./MyProfileStyle.css');
+  require('./Style.css');
+  require('./MainStyle.css');
+
+  return (
+    <div>
+      <div className="layout">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+          <div className="container">
+            <a
+              className="navbar-brand js-scroll-trigger"
+              href="/mainpageforloggedin">
+              WhiteWall
+            </a>
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              type="button" data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              Menu
+              <i className="fas fa-bars"/>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <div className="btn-group">
+                  <Link to="/addphoto" className="btn bg-primary light">Dodaj zdjęcie</Link>
+                  <Link to="/logout" className="btn bg-primary light">Wyloguj się</Link>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row profile">
+            <div className="col-md-3">
+              <div className="profile-sidebar">
+                <div className="profile-usertitle">
+                  <div className="profile-usertitle-name">
+                    <h2>{username}</h2>
                   </div>
-                </ul>
+                </div>
+                <div className="profile-userbuttons">
+                  {isFollowed()}
+                </div>
+                <div className="profile-usermenu">
+                  <ul className="nav">
+                    <li className="active">
+                      <a href="#">
+                        <i className="glyphicon glyphicon-home"/>
+                        Przegląd </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </nav>
-          <div className="container">
-            <div className="row profile">
-              <div className="col-md-3">
-                <div className="profile-sidebar">
-                  <div className="profile-usertitle">
-                    <div className="profile-usertitle-name">
-                      <h2>{this.state.username}</h2>
-                    </div>
-                  </div>
-                  <div className="profile-userbuttons">
-                    {this.isFollowed()}
-                  </div>
-                  <div className="profile-usermenu">
-                    <ul className="nav">
-                      <li className="active">
-                        <a href="#">
-                          <i className="glyphicon glyphicon-home"></i>
-                          Przegląd </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-9">
-                <div className="profile-content">
-                  {this.renderAllPhotos()}
-                </div>
+            <div className="col-md-9">
+              <div className="profile-content">
+                {renderAllPhotos()}
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 
+export default Profile
