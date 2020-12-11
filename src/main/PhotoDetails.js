8575e985ef7faf6b9844ce120d744eac5553c9a8
White Joan <joannnabiala@gmail.com>
2020-12-11 18:23:17 +0100

    Layout changes for PhotoDetails.js

--- src/main/PhotoDetails.js
+++ src/main/PhotoDetails.js
@@ -38,7 +38,6 @@ const PhotoDetails = () => {
 
 
   const handleSubmitComment = (event) => {
-
     const data = {
       content: content,
       photo: photoId
@@ -73,21 +72,15 @@ const PhotoDetails = () => {
 
   const commentPattern = (comment) => {
     return (
-      <div>
-        <div className="well well-lg">
-          <h4 className="media-heading text-uppercase reviews">
-            {renderCommentAuthorLinkTo(comment)}
-          </h4>
-          <ul className="media-date text-uppercase reviews list-inline">
-            <Moment format="YYYY/MM/DD">
-              {photoMeta.creationTime}
-            </Moment>
-          </ul>
-          <p className="media-comment">
-            {comment.content}
-          </p>
-        </div>
-      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">
+          {renderCommentAuthorLinkTo(comment)}
+          <Moment format="YYYY/MM/DD">
+            {photoMeta.creationTime}
+          </Moment>
+          {comment.content}
+        </li>
+      </ul>
     );
   }
 
@@ -169,109 +162,111 @@ const PhotoDetails = () => {
     <PageTemplateComponent>
       <LeftSideNavBarComponent/>
       <CenterComponent>
-        <div className="card-body">
-          <Link className="col-md-10 thumbnail">
-            <img src={photoSrc} alt="Lights"/>
-
-            <div className="card-body">
-              <div className="well well-lg light">
-                <h4 className="media-heading text-uppercase reviews">
+        <div id="photo-details-photo" className="row">
+          <div className="col-8">
+            <div className="image-container">
+              <img src={photoSrc} alt="Lights"/>
+            </div>
+          </div>
+          <div id="photo-details-row" className="col-4">
+            <div className="row comments">
+              <ul className="list-group w-100">
+                <li id="main-username" className="list-group-item">
                   {renderAuthorLinkTo()}
-                </h4>
-                <div>
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={() => handleLikeUnlikeClick(photoId)}
-                    id={photoId}>
-                    {photoMeta.isLikedByMe ?
-                      <svg
-                        className="bi bi-heart"
-                        width="1em" height="1em"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C12.72-3.042 23.333 4.867 8 15z"
-                          clip-rule="evenodd"/>
-                      </svg>
-                      :
-                      <svg
-                        className="bi bi-heart-fill"
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-                          clip-rule="evenodd"/>
-                      </svg>
-                    }
-                  </button>
-                  {photoMeta.likesAmount}
-                </div>
-                <svg
-                  className="bi bi-chat"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 16 16"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill-rule="evenodd"
-                    d="M2.678 11.894a1 1 0 01.287.801 10.97 10.97 0 01-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 01.71-.074A8.06 8.06 0 008 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 01-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 00.244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 01-2.347-.306c-.52.263-1.639.742-3.468 1.105z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-
-                <p>{photoMeta.comments.length}</p>
-
-                <ul className="media-date text-uppercase reviews list-inline">
+                </li>
+                <hl/>
+                <li id="photo-details-list" className="list-group-item">
+                  {renderAuthorLinkTo()}
+                  <div>----------</div>
+                  <hl/>
+                  {photoMeta.description}
+                  <div>----------</div>
                   <Moment format="YYYY/MM/DD">
                     {photoMeta.creationTime}
                   </Moment>
-                </ul>
+                  <div>-----------</div>
+                  {photoMeta.isMe ? <button
+                    type="button"
+                    className="btn bg-primary light"
+                    onClick={() => deletePhoto()}>
+                    Usuń
+                  </button> : null}
 
-                <p className="card-text text-justify">
-                  {photoMeta.description}
-                </p>
-
-                {photoMeta.isMe ? <button
-                  type="button"
-                  className="btn bg-primary light"
-                  onClick={() => deletePhoto()}>
-                  Usuń
-                </button> : null}
-              </div>
-              <div className="media-body">
-                <div className="form-group">
-                  <label htmlFor="exampleFormControlTextarea1">
-                    Dodaj komentarz:
-                  </label>
+                  {renderAllComments()}
+                </li>
+                <hl/>
+                <li id="photo-details-likes-and-comments" className="list-group-item">
+                  <div className="row">
+                    <button
+                      type="button"
+                      className=""
+                      onClick={() => handleLikeUnlikeClick(photoId)}
+                      id={photoId}>
+                      {photoMeta.isLikedByMe ?
+                        <svg
+                          className="bi bi-heart"
+                          width="1em" height="1em"
+                          viewBox="0 0 16 16"
+                          fill="currentColor"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            fill-rule="evenodd"
+                            d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C12.72-3.042 23.333 4.867 8 15z"
+                            clip-rule="evenodd"/>
+                        </svg>
+                        :
+                        <svg
+                          className="bi bi-heart-fill"
+                          width="1em"
+                          height="1em"
+                          viewBox="0 0 16 16"
+                          fill="currentColor"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            fill-rule="evenodd"
+                            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                            clip-rule="evenodd"/>
+                        </svg>
+                      }
+                    </button>
+                    <div>
+                      {photoMeta.likesAmount}
+                    </div>
+                    <svg
+                      className="bi bi-chat"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        fill-rule="evenodd"
+                        d="M2.678 11.894a1 1 0 01.287.801 10.97 10.97 0 01-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 01.71-.074A8.06 8.06 0 008 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 01-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 00.244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 01-2.347-.306c-.52.263-1.639.742-3.468 1.105z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    <p>{photoMeta.comments.length}</p>
+                  </div>
+                </li>
+                <hl/>
+                <li id="photo-details-textarea" className="list-group-item">
+                  Dodaj komentarz:
                   <textarea
-                    className="form-control"
-                    id="exampleFormControlTextarea1"
                     rows="4"
                     value={content}
                     onChange={(e) => setContent(e.target.value)}
                   />
-                  <button className="btn bg-primary light" onClick={handleSubmitComment}>
+                  <button onClick={handleSubmitComment}>
                     Dodaj
                   </button>
-                </div>
-              </div>
+                </li>
+              </ul>
             </div>
-            <div>
-              {renderAllComments()}
-            </div>
-          </Link>
+          </div>
         </div>
       </CenterComponent>
+      <RightSideComponent/>
     </PageTemplateComponent>
-
   )
 }
 
